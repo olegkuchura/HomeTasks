eeee25a8ad1fe86f1e8091e2b572365d39fb4a8e
oleg <olegkuchura@ukr.net>
2017-07-11 07:58:15 +0300

    Some Changes

--- app/src/main/java/com/bignerbranch/android/firsthometask/HelloInternship.java
+++ app/src/main/java/com/bignerbranch/android/firsthometask/HelloInternship.java
@@ -2,6 +2,9 @@ package com.bignerbranch.android.firsthometask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HelloInternship extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HelloInternship extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_internship);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button was pressed", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 }
